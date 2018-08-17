7fbc20cdee0ba48b15ac10a24dd63a14d518c809
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-08-17 10:29:46 +0500

    Update Balancing data.R

--- Dataset/Balancing data.R
+++ Dataset/Balancing data.R
@@ -20,13 +20,14 @@ library(seqinr)
 library(Peptides)
 library(AUC)
 library(ROCR)
+library(prospectr)
 
 x1 <-read.fasta('AFP.fasta', seqtype="AA", as.string = TRUE)
 x2<- read.fasta('nonAFP.fasta', seqtype="AA", as.string = TRUE)
 
 x1 <- x1[(sapply(x1, protcheck))]
 x2 <- x2[(sapply(x2, protcheck))]
-label = c(rep("AFP", length(x1)),rep("nonTHP", length(x2)))
+label = c(rep("AFP", length(x1)),rep("nonAFP", length(x2)))
 
 AAC1 <- t(sapply(x1, extractAAC))
 AAC2 <- t(sapply(x2, extractAAC))
@@ -34,11 +35,11 @@ AAC = rbind(AAC1,AAC2)
 
 df = data.frame(scale(AAC),Class = label)
 data = df[!duplicated(df), ]
-internal = data[,!sapply(data,function(x) any(is.na(x)))]
+data2 = data[,!sapply(data,function(x) any(is.na(x)))]
                          
 ################ Random
-Pos = read.csv("ALL-AFP.csv", header = TRUE) 
-Neg = read.csv("ALL-nonAFP.csv", header = TRUE)
+Pos = subset(data2, Class == 'AFP')
+Neg = subset(data2, Class == 'nonAFP')
 
 #######  Dividing Training and Testing sets on positive and negative classes
 sample1 <- c(sample(1:nrow(Pos) ,300))
@@ -51,6 +52,14 @@ sample2 <- c(sample(1:nrow(Neg),300))
   TR <- rbind(train1,train2) ####combining for internal set
   TS <- rbind(test1,test2)    ####combining for external set
 
+################ Kennard-Stone algorithm
+  
+npos = round(nrow(Pos)*0.75)
+nneg = round(nrow(Neg)*0.75)                                     
+kenX <- kenStone(Pos[,-ncol(Pos)],k=npos,metric='euclid',pc=5)
+kenY <- kenStone(Neg[,-ncol(Neg)],k=nneg,metric='euclid',pc=5)
 
-
-           
+internal = rbind(Pos[kenX$model,], Neg[kenX$model,])                     
+external = rbind(Pos[kenY$test,], Neg[kenY$test,])         
+                      
+                      
